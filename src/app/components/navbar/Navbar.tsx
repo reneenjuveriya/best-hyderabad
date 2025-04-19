53399f2325ee54bc1931e91ac0a3d856c2ab2e5e
Reneen <reneenjuveriya@gmail.com>
2025-04-20 00:10:22 +0530

    removed unused var

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -5,7 +5,6 @@ import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 import { SafeUser } from "@/app/types";
-import Categories from "./Categories";
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
